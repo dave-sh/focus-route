a58c8719783e82079a2091a00150e703cf030f6d
ymelul342 <ymelul342@gmail.com>
2022-03-27 17:51:47 -0400

    sign -in page done

--- project/src/pages/sign_in/Sign_In.jsx
+++ project/src/pages/sign_in/Sign_In.jsx
@@ -3,7 +3,22 @@ import './sign_in.css'
 
 const Sign_In = () => {
   return (
-    <div className='sign_in'>Sign_In</div>
+    <div className='sign_in'>
+      <div className ='sign_in_form'>
+        <h1>Welcome Back</h1>
+        <h4>please sign in to continue!</h4>
+        <input placeholder='Email' />
+        <input type="password" placeholder='Password' />
+      
+      <p><a href="#">Forgot Password?</a></p>
+      <button className='sign_in_button 'type='submit'>Login</button>
+      <p>Dont have an Account?
+        <a className="sign_in_form_sign_up" href="#"> sign up</a>
+      </p>
+      </div>
+      
+
+    </div>
   )
 }
 
